250b1ec04cf7535f5cb29041a435ad2b93ada0be
Elder <lopez_felix_23@hotmail.com>
2017-03-14 11:33:03 -0600

    Proyecto inicial de Angular 2 Con Gulp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,16 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
 
 import {AppComponent} from "./app.component";
-import {TaskListComponent} from "./todo/components/task-list.component";
-import {AboutComponent} from "./about/components/about.component";
-import {TaskComponent} from "./todo/components/task.component";
-
-import {routing, appRoutingProviders} from './app.routing';
-import {FormsModule} from "@angular/forms";
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        routing
     ],
     declarations: [
         AppComponent,
-        TaskComponent,
-        TaskListComponent,
-        AboutComponent
-    ],
-    providers: [
-        appRoutingProviders
     ],
     bootstrap: [AppComponent]
 })
